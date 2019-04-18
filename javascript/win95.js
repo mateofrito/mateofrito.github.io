e21e62a8da1329ef3965e43c6af318d4c082f850
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-17 20:14:53 -0400

    windows95 portfolio

---
+++ javascript/win95.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  // your code
+
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("start");
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
